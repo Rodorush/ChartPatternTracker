6ca90074155c1f399b267839a1ecd124feb2f728
Rodolfo Pereira de Andrade <rodolfop.andrade@gmail.com>
2024-10-28 09:42:19 -0300

    feat(ui): Criando menu principal
    • Criando botões
    • Tradução para inglês e espanhol

--- app/src/main/java/br/com/rodorush/chartpatterntracker/MainActivity.kt
+++ app/src/main/java/br/com/rodorush/chartpatterntracker/MainActivity.kt
@@ -1,16 +1,40 @@
 package br.com.rodorush.chartpatterntracker
 
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.rodorush.chartpatterntracker.ui.theme.ChartPatternTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +44,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ChartPatternTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MainScreen(
+                        padding = innerPadding
                     )
                 }
             }
@@ -31,17 +54,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(padding: PaddingValues) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(padding)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(R.string.main_menu),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        ButtonMenuItem(R.string.pattern_tracker, context)
+        ButtonMenuItem(R.string.financial_news, context)
+        ButtonMenuItem(R.string.real_time_quotes, context)
+        ButtonMenuItem(R.string.technical_indicators, context)
+        Spacer(modifier = Modifier.height(20.dp))
+        TextButton(onClick = { }) {
+            Text(text = stringResource(R.string.logout))
+            Icon(imageVector = Icons.AutoMirrored.Filled.ExitToApp, contentDescription = null)
+        }
+    }
+}
+
+@Composable
+fun ButtonMenuItem(texResId: Int, context: Context) {
+    Button(
+        onClick = {
+            Toast.makeText(context, context.getString(texResId), Toast.LENGTH_SHORT).show()
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(vertical = 8.dp)
+    ) {
+        Text(text = stringResource(texResId))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     ChartPatternTrackerTheme {
-        Greeting("Android")
+        MainScreen(padding = PaddingValues(0.dp))
     }
 }
