84ef85123417a20c097cfaf4318dfdd1a8896d69
Rodolfo Pereira de Andrade <rodolfop.andrade@gmail.com>
2024-12-11 08:55:26 -0300

    feat(ui): Construindo layout Authenticaion Activity
    • Inserindo e posicionando elementos
    • Gradle atualizado

--- app/src/main/java/br/com/rodorush/chartpatterntracker/activities/AuthenticationActivity.kt
+++ app/src/main/java/br/com/rodorush/chartpatterntracker/activities/AuthenticationActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -20,6 +28,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import br.com.rodorush.chartpatterntracker.R
 import br.com.rodorush.chartpatterntracker.ui.theme.ChartPatternTrackerTheme
 import br.com.rodorush.chartpatterntracker.viewmodels.AuthenticationViewModel
 
@@ -62,8 +71,15 @@ fun LoginScreen(onNavigateToRegister: () -> Unit) {
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = stringResource(id = R.string.app_name),
+            modifier = Modifier
+                .size(120.dp)
+                .offset(y = 94.dp),
+        )
         Text(text = "Login")
         Button(onClick = onNavigateToRegister) {
             Text(text = "Ir para Cadastro")
