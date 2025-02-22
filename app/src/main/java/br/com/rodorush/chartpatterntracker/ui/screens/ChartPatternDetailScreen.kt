cbf46c8e29dda178c9fb8710836a68c20b820c1d
Rodolfo Pereira de Andrade <rodolfop.andrade@gmail.com>
2025-02-22 18:45:28 -0300

    feat(ui): Ajuste nos textos da tela de detalhes
    • Texto de descrição do padrão, quando muito longo, agora pode ser rolado

--- app/src/main/java/br/com/rodorush/chartpatterntracker/ui/screens/ChartPatternDetailScreen.kt
+++ app/src/main/java/br/com/rodorush/chartpatterntracker/ui/screens/ChartPatternDetailScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.BottomAppBar
@@ -38,6 +40,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.rodorush.chartpatterntracker.R
@@ -199,13 +202,29 @@ fun ChartPatternDetailScreen(
                             style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
                         )
                         Text(
-                            text = "Confiança: ${pattern.getLocalized("reliability")}",
+                            text = stringResource(
+                                R.string.reliability,
+                                pattern.getLocalized("reliability")
+                            ),
                             style = MaterialTheme.typography.bodySmall.copy(color = Color.Gray)
                         )
-                        Text(
-                            text = pattern.getLocalized("description"),
-                            style = MaterialTheme.typography.bodyMedium
-                        )
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .verticalScroll(rememberScrollState())
+                            ) {
+                                Text(
+                                    text = pattern.getLocalized("description"),
+                                    textAlign = TextAlign.Justify,
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
+                        }
                     }
                 }
             }
