89d8f2606ca78f7613ee78fb1e8a559fa357638d
Rodorush <rodolfop.andrade@gmail.com>
2025-07-16 22:03:21 -0300

    Map indication strings to icons

--- app/src/main/java/br/com/rodorush/chartpatterntracker/model/ScreeningResult.kt
+++ app/src/main/java/br/com/rodorush/chartpatterntracker/model/ScreeningResult.kt
@@ -1,12 +1,10 @@
 package br.com.rodorush.chartpatterntracker.model
 
-import br.com.rodorush.chartpatterntracker.R
-
 data class ScreeningResult(
     val pattern: PatternItem,
     val asset: AssetItem,
     val timeframe: TimeframeItem,
     val reliability: String,
     val indication: String,
-    val indicationIcon: Int = R.drawable.ic_up_arrow
+    val indicationIcon: Int
 )
